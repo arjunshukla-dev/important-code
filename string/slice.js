d2fedc0d0970618e54ebf519b7fb68b1527f4075
Arjun Shukla <arjunbhadnang@gmail.com>
2024-03-06 00:05:10 +0530

    other js file

---
+++ string/slice.js
@@ -0,0 +1,15 @@
+var str = "Arjun Shukla Unnao";
+//console.log(str.substring(2,5));
+//console.log(str.slice(2,5))
+//console.log(str.slice(-5,-1))
+
+//console.log(str.substr(2,18));
+//console.log(str.length);
+//console.log(str.split(" "));
+console.log(str.charAt(0))
+var str1 = "abc.xyz.jpg";
+var str2 = "syz.jpg";
+var ext = str1.substring(str1.lastIndexOf(".")) // .jpg
+console.log(ext);
+console.log(str1.lastIndexOf("."))
+
